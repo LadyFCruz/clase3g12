db5c17ccd18aaace20be994ecf28d40640233bb0
LadyFCruz <ladycruz525@gmail.com>
2022-11-24 20:31:14 -0500

    first commit

---
+++ src/componentes/CrearCuenta.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const CrearCuenta = () =>{
+    
+    return (
+        <div>
+          
+          <h1> Crear cuenta </h1>
+          <h2> Ingrese los datos del usuario: </h2>
+          <input placeholder ='Nombre' />
+          <input placeholder ='Email'/>
+          <input placeholder ='Password' />
+          <input placeholder ='Confirmar' />
+          
+          <button>Crear Cuenta</button>
+          <Link to={"/"}>Regresar</Link>
+        </div>
+        
+      );
+    }
+
+  
+
+export default CrearCuenta;
